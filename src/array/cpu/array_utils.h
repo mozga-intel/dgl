0676247d546dffc0efc3286490f1fdb90e149497
mozga-intel <mateusz.ozga@intel.com>
2020-07-14 10:54:32 +0200

    Fnv1 hash_function

--- src/array/cpu/array_utils.h
+++ src/array/cpu/array_utils.h
@@ -13,6 +13,41 @@
 #include <utility>
 #include "../../c_api_common.h"
 
+
+class FnvHash
+{
+	static const unsigned int FNV_PRIME = 16777619u;
+        static const unsigned int OFFSET_BASIS = 2166136261u;
+        template <unsigned int N>
+        static constexpr unsigned int fnvHashConst(const char (&str)[N], unsigned int I = N)
+        {
+            return I == 1 ? (OFFSET_BASIS ^ str[0]) * FNV_PRIME : (fnvHashConst(str, I - 1) ^ str[I - 1]) * FNV_PRIME;
+        }
+        static unsigned int fnvHash(const char* str)
+        {
+            const size_t length = strlen(str) + 1;
+            unsigned int hash = OFFSET_BASIS;
+            for (size_t i = 0; i < length; ++i)
+            {
+                hash ^= *str++;
+                hash *= FNV_PRIME;
+            }
+            return hash;
+        }
+        struct Wrapper
+        {
+            Wrapper(const char* str) : str (str) { }
+            const char* str;
+        };
+        unsigned int hash_value;
+    public:
+        explicit FnvHash(Wrapper wrapper) : hash_value(fnvHash(wrapper.str)) { }
+        template <unsigned int N>
+        constexpr FnvHash(const char (&str)[N]) : hash_value(fnvHashConst(str)) { }
+        constexpr operator unsigned int() const { return this->hash_value; }
+};
+
+
 namespace dgl {
 namespace aten {
 
@@ -79,7 +114,7 @@ class IdHashMap {
   IdArray Map(IdArray ids, IdType default_val) const {
     const IdType* ids_data = static_cast<IdType*>(ids->data);
     const int64_t len = ids->shape[0];
-    IdArray values = NewIdArray(len, ids->ctx, ids->dtype.bits);
+ bsl::btree_set<   IdArray values = NewIdArray(len, ids->ctx, ids->dtype.bits);
     IdType* values_data = static_cast<IdType*>(values->data);
     for (int64_t i = 0; i < len; ++i)
       values_data[i] = Map(ids_data[i], default_val);
